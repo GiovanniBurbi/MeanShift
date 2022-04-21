c2ddd8ac21cdcf5c83a29012340c8ccdb26a2387
Giovanni <giovanniburbi10@gmail.com>
2022-04-21 20:19:34 +0200

    Launcher refactored

--- mean-shift/src/main/java/com/apt/project/mean_shift/App.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/App.java
@@ -1,11 +1,11 @@
 package com.apt.project.mean_shift;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.apt.project.mean_shift.algorithm.MeanShift;
 import com.apt.project.mean_shift.model.Point;
 import com.apt.project.mean_shift.utils.CSVParser;
+import com.apt.project.mean_shift.utils.ColorConverter;
 import com.apt.project.mean_shift.utils.ImageParser;
 
 public class App 
@@ -21,12 +21,12 @@ public class App
     	CSVParser p = new CSVParser();
     	ImageParser ip = new ImageParser("/images/benzina.jpg");
     	List<Point<Integer>> rgbPoints = ip.extractRGBPoints();
-    	List<Point<Double>> luvPoints = ip.convertToLUVPoints(rgbPoints);
+    	List<Point<Double>> luvPoints = ColorConverter.convertToLUVPoints(rgbPoints);
     	p.write(luvPoints, "originPoints.csv");
     	MeanShift meanShift = new MeanShift(15f, 5, luvPoints);
-    	ArrayList<Point<Double>> shiftedPoints = meanShift.meanShiftAlgorithm();
+    	List<Point<Double>> shiftedPoints = meanShift.meanShiftAlgorithm();
     	p.write(shiftedPoints, "shiftedPoints.csv");
-    	List<Point<Integer>> rgbShiftedPoints = ip.convertToRGBPoints(shiftedPoints);
+    	List<Point<Integer>> rgbShiftedPoints = ColorConverter.convertToRGBPoints(shiftedPoints);
     	ip.renderImage(rgbShiftedPoints, "results/resultBenzinaBW15Iter5.jpg");
     	
     	
@@ -34,11 +34,10 @@ public class App
     	
 //    	CSVParser p = new CSVParser();
 //    	p.fetchCSVFile("/points/100.csv");
-//    	ArrayList<Point> points = p.extractPoints();
+//    	List<Point<Double>> points = p.extractPoints();
 //    	MeanShift meanShift = new MeanShift(2, 10, points);
-//    	ArrayList<Point> shiftedPoints = meanShift.meanShiftAlgorithm();
-//    	p.write(shiftedPoints);
-//    	p.printPoints();
+//    	List<Point<Double>> shiftedPoints = meanShift.meanShiftAlgorithm();
+//    	p.write(shiftedPoints, "shiftedPoints.csv");
     	
     	
 //    	Parallel version
