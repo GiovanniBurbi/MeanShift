eeed973322b21d5f6e31def4fe9ed6e99e868731
Giovanni <giovanniburbi10@gmail.com>
2022-04-22 15:48:07 +0200

    Logger + compute times as average of few runs

--- mean-shift/src/main/java/com/apt/project/mean_shift/App.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/App.java
@@ -1,6 +1,7 @@
 package com.apt.project.mean_shift;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.apt.project.mean_shift.algorithm.MeanShift;
 import com.apt.project.mean_shift.model.Point;
@@ -10,7 +11,10 @@ import com.apt.project.mean_shift.utils.ImageParser;
 
 public class App 
 {
-    public static void main( String[] args )
+	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+	private static final int ITER = 3;
+
+	public static void main( String[] args )
     {
     	
 //    	Sequential version
@@ -24,7 +28,20 @@ public class App
     	List<Point<Double>> luvPoints = ColorConverter.convertToLUVPoints(rgbPoints);
 //    	p.write(luvPoints, "originPoints.csv");
     	MeanShift meanShift = new MeanShift(12f, 5, luvPoints);
+    	long startTime;
+    	long endTime;
+    	long allTimes = 0;
+    	for (int i = 0; i < ITER - 1; i++) {
+    		startTime = System.currentTimeMillis();
+    		meanShift.meanShiftAlgorithm();
+    		endTime = System.currentTimeMillis();
+    		allTimes += endTime - startTime;
+    	}
+    	startTime = System.currentTimeMillis();
     	List<Point<Double>> shiftedPoints = meanShift.meanShiftAlgorithm();
+		endTime = System.currentTimeMillis();
+		allTimes += endTime - startTime;
+    	LOGGER.info("Sequential version took " + (allTimes / ITER) + " milliseconds");
 //    	p.write(shiftedPoints, "shiftedPoints.csv");
     	List<Point<Integer>> rgbShiftedPoints = ColorConverter.convertToRGBPoints(shiftedPoints);
     	ip.renderImage(rgbShiftedPoints, "results/resultBenzinaBW12Iter5.jpg");
