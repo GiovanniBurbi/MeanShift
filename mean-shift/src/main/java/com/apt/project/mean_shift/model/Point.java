0546c279ede09a6045408dcdee602e82feeefe7c
Giovanni <giovanniburbi10@gmail.com>
2022-04-22 15:46:52 +0200

    copy costructor

--- mean-shift/src/main/java/com/apt/project/mean_shift/model/Point.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/model/Point.java
@@ -11,23 +11,22 @@ public class Point<T> {
 		this.d3 = d3;
 	}
 	
+	public Point(Point<T> point) {
+		this.d1 = point.d1;
+		this.d2 = point.d2;
+		this.d3 = point.d3;
+	}
+	
 	public T getD1() {
 		return d1;
 	}
-	public void setD1(T d1) {
-		this.d1 = d1;
-	}
+
 	public T getD2() {
 		return d2;
 	}
-	public void setD2(T d2) {
-		this.d2 = d2;
-	}
+
 	public T getD3() {
 		return d3;
-	}
-	public void setD3(T d3) {
-		this.d3 = d3;
 	}
 	
 	@Override
