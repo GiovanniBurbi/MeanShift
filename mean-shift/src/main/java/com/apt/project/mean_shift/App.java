c94fafa768613b71e2b5bf1ca4d004c8fde25aec
Giovanni <giovanniburbi10@gmail.com>
2022-04-23 16:45:20 +0200

    threads with CountDownLatch, to review

--- mean-shift/src/main/java/com/apt/project/mean_shift/App.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/App.java
@@ -2,13 +2,18 @@ package com.apt.project.mean_shift;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.apt.project.mean_shift.algorithm.MeanShift;
 import com.apt.project.mean_shift.model.Point;
+import com.apt.project.mean_shift.parallel.MeanShiftThread;
 import com.apt.project.mean_shift.utils.ColorConverter;
 import com.apt.project.mean_shift.utils.ImageParser;
 
@@ -72,30 +77,65 @@ public class App
     	long endTime;
     	long allTimes = 0;
 
-    	ExecutorService exec = Executors.newFixedThreadPool(N_THREAD);
+    	ExecutorService executor = Executors.newFixedThreadPool(N_THREAD);
     	int numberOfElements = luvPoints.size();
     	int minElementsPerThread = numberOfElements / N_THREAD;
     	int threadsWithMaxElements = numberOfElements - N_THREAD * minElementsPerThread;
     	int startIndex = 0;
     	int nElements;
     	int endIndex;
-    	//List<Future<?>> futures = new ArrayList<Future<?>>(N_THREAD); // ? Is it needed? maybe i only need to implement a runnable and not a callable
+//    	List<Future<?>> futures = new ArrayList<Future<?>>(N_THREAD);
+//    	
+//    	for (int k = 0; k < ITER; k++) {
+//    		startTime = System.currentTimeMillis();
+//			for (int i = 0; i < N_THREAD; i++) {
+				// TODO do these operation (and line 78) inside thread
+//				nElements = (i < threadsWithMaxElements) ? minElementsPerThread + 1 : minElementsPerThread;
+//				endIndex = startIndex + nElements;
+//				futures.add(executor.submit(new MeanShiftThread()));
+//				startIndex = endIndex;
+//			}
+//			
+//			for (int i = 0; i < futures.size(); i++) {
+//				try {
+//					futures.get(i).get();
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (ExecutionException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
     	
+    	CountDownLatch latch = new CountDownLatch(N_THREAD);
     	for (int k = 0; k < ITER; k++) {
     		startTime = System.currentTimeMillis();
 			for (int i = 0; i < N_THREAD; i++) {
-				// TODO do these operation (and line 78) inside thread
+// TODO do these operation (and line 78) inside thread
 				nElements = (i < threadsWithMaxElements) ? minElementsPerThread + 1 : minElementsPerThread;
 				endIndex = startIndex + nElements;
-// start threads TODO
+				executor.execute(new MeanShiftThread(latch));
 				startIndex = endIndex;
 			}
-// wait threads to finish TODO
+			
+			try {
+				latch.await();
+			} catch (InterruptedException e) {
+				LOGGER.log(Level.WARNING, e.getMessage(), e);
+			}
 			
 			endTime = System.currentTimeMillis();
 			allTimes += endTime - startTime;
     	}
-		exec.shutdown();
+		executor.shutdown();
+		try {
+		    if (!executor.awaitTermination(2, TimeUnit.SECONDS)) {
+		        executor.shutdownNow();
+		    } 
+		} catch (InterruptedException e) {
+		    executor.shutdownNow();
+		}
     	LOGGER.info("Parallel version took " + (allTimes / ITER) + " milliseconds");
     	List<Point<Integer>> rgbShiftedPoints = ColorConverter.convertToRGBPoints(shiftedPoints);
     	ip.renderImage(rgbShiftedPoints, "results/resultBenzinaBW12Iter5Parallel.jpg");
--- mean-shift/src/main/java/com/apt/project/mean_shift/parallel/MeanShiftThread.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/parallel/MeanShiftThread.java
@@ -1,14 +1,16 @@
 package com.apt.project.mean_shift.parallel;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import com.apt.project.mean_shift.model.Point;
 
 public class MeanShiftThread implements Runnable{
 	
 	private List<Point<Double>> shiftedPoints;
+	private CountDownLatch latch;
 	
-	public MeanShiftThread() {
+	public MeanShiftThread(CountDownLatch latch) {
 		// TODO initialize list with the size of the chuck
 		// TODO copy chunk from origin and write it on local list (try directly on result list)
 	}
@@ -16,6 +18,7 @@ public class MeanShiftThread implements Runnable{
 	@Override
 	public void run() {
 		// TODO
+		latch.countDown();
 		
 	}
 
