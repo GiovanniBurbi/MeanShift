3dd64c4abbb4eb26f6a95a6084299cc66ba661a6
Giovanni <giovanniburbi10@gmail.com>
2022-04-24 14:36:06 +0200

    removed parallel version from mean shift class

--- mean-shift/src/main/java/com/apt/project/mean_shift/algorithm/MeanShift.java
+++ mean-shift/src/main/java/com/apt/project/mean_shift/algorithm/MeanShift.java
@@ -74,21 +74,4 @@ public class MeanShift {
 		}
 		return shiftedPoints;
 	}
-	
-	
-	public List<Point<Double>> meanShiftAlgorithmParallel(int startIndex, int endIndex) {
-		ArrayList<Point<Double>> shiftedPoints = new ArrayList<>(endIndex - startIndex);
-// deep copy of origin points
-		for (int i = startIndex; i < endIndex; i++) {
-			shiftedPoints.add(new Point<>(originPoints.get(i)));
-		}
-// algorithm main loop
-		for(int i = 0; i < this.maxIter; i++) {
-			LOGGER.info("iterazione: " + i);
-			for (int j = 0; j < shiftedPoints.size(); j++) {
-				shiftedPoints.get(j).replace(this.shiftPoint(shiftedPoints.get(j)));
-			}
-		}
-		return shiftedPoints;
-	}
 }
